a8e37da82ae42c86bdf6edcf3a164976035c5a0b
AnyaLisna <anyalesnaya2812@gmail.com>
2019-06-06 23:35:22 +0300

    Updates

---
+++ precache-manifest.ab21ee5d671afca04dc29ed0c11862eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca10f2d7f4f0d093fc48198bd9082793",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "c227803b811eec0a547d",
+    "url": "/react_dynamic-list-of-todos/static/css/main.d1397bba.chunk.css"
+  },
+  {
+    "revision": "22a965e88c80956f5e2b",
+    "url": "/react_dynamic-list-of-todos/static/js/2.a99c8d40.chunk.js"
+  },
+  {
+    "revision": "c227803b811eec0a547d",
+    "url": "/react_dynamic-list-of-todos/static/js/main.8b5664ae.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
